acd69881e82ff3735af2548cf9cf55166aa5c8d2
Karol Szafrański <kszafranski@eclipsesource.com>
2018-06-11 17:25:03 +0200

    create matrix of views

--- layout-performance/ViewController.m
+++ layout-performance/ViewController.m
@@ -16,7 +16,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    UIView* v;
+    int l = 35;
+
+    for (int i = 0; i < l; i++) {
+        for (int k = 0; k < l; k++) {
+            v = [[UIView alloc] initWithFrame:CGRectMake(5 + (i * 15), 5 + (k * 15), 10, 10)];
+            v.backgroundColor = [UIColor grayColor];
+            [self.view addSubview:v];
+
+            // absolute positioning
+            v.translatesAutoresizingMaskIntoConstraints = NO;
+            [v.topAnchor constraintEqualToAnchor:v.superview.topAnchor constant:5 + (i * 15)].active = YES;
+            [v.leftAnchor constraintEqualToAnchor:v.superview.leftAnchor constant:5 + (k * 15)].active = YES;
+            [v.widthAnchor constraintEqualToConstant:10.0].active = YES;
+            [v.heightAnchor constraintEqualToConstant:10.0].active = YES;
+        }
+    }
 }
 
 
